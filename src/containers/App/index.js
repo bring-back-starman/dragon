2887332b200ad50bd174fdc575396a0cf9792c6a
Antoine Gaillot <manzagregine@gmail.com>
2018-03-16 15:05:14 +0100

    Refactoring index.js and App component

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,38 +1,17 @@
 // @flow
 import React, { PureComponent } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
-import { ConnectedRouter as Router } from 'react-router-redux';
-import { createBrowserHistory } from 'history';
-import { Provider } from 'react-redux';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
-
-import configureStore from 'store/configureStore';
 
 import HomePage from 'containers/HomePage';
-
-// Pass your GraphQL endpoint to uri
-const client = new ApolloClient({
-  uri: 'https://nx9zvp49q7.lp.gql.zone/graphql',
-});
-const initialState = {};
-const history = createBrowserHistory();
-const store = configureStore(initialState, history);
 
 class App extends PureComponent<void> {
   render() {
     return (
-      <Provider store={store}>
-        <ApolloProvider client={client}>
-          <Router history={history}>
-            <Switch>
-              <Redirect exact to="/missions/upcoming" from="/" />
-              <Route path="/missions/:filter" component={HomePage} />
-              <Route />
-            </Switch>
-          </Router>
-        </ApolloProvider>
-      </Provider>
+      <Switch>
+        <Redirect exact to="/missions/upcoming" from="/" />
+        <Route path="/missions/:filter" component={HomePage} />
+        <Route />
+      </Switch>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import { createBrowserHistory } from 'history';
+import { ConnectedRouter as Router } from 'react-router-redux';
+
+import configureStore from 'store/configureStore';
 import App from 'containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
 import 'index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Pass your GraphQL endpoint to uri
+const client = new ApolloClient({
+  uri: 'http://localhost:3000/graphql',
+});
+const history = createBrowserHistory();
+const initialState = {};
+const store = configureStore(initialState, history);
+
+const MOUNT_NODE = document.getElementById('app');
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ApolloProvider client={client}>
+      <Router history={history}>
+        <App />
+      </Router>
+    </ApolloProvider>
+  </Provider>,
+  MOUNT_NODE,
+);
 registerServiceWorker();
