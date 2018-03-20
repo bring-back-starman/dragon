ef57243d688d5efcf0401bc0593fea05a70bfac0
Antoine Gaillot <manzagregine@gmail.com>
2018-03-20 17:41:29 +0100

    Adding global layout

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,15 +3,17 @@ import React, { PureComponent } from 'react';
 import { Redirect, Route, Switch, withRouter } from 'react-router-dom';
 
 import HomePage from 'containers/HomePage/index';
+import Layout from 'components/Layout';
 
 class App extends PureComponent<void> {
   render() {
     return (
-      <Switch>
-        <Redirect exact to="/missions/upcoming" from="/" />
-        <Route path="/missions/:type" component={HomePage} />
-        <Route />
-      </Switch>
+      <Layout>
+        <Switch>
+          <Redirect exact to="/missions/upcoming" from="/" />
+          <Route path="/missions/:type" component={HomePage} />
+        </Switch>
+      </Layout>
     );
   }
 }
---
+++ src/components/Layout/index.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components/typings/styled-components';
+
+const Layout = styled.div`
+  background-color: ${props => props.theme.colors.secondary};
+  min-height: 100vh;
+`;
+
+export default Layout;
